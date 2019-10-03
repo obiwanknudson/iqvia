80a1187cf42cf9fffe9909132b47a965490133a8
Knudson <bknudson@SEAW3029353.local>
2019-10-03 15:56:19 -0700

    Updates

---
+++ precache-manifest.0dc67b205f3a63340db8e38ef5996fae.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd528107a0bb7e02e272dbfdf1dc9e71",
+    "url": "/iqvia/index.html"
+  },
+  {
+    "revision": "f9e3753e07529f05f1d9",
+    "url": "/iqvia/static/css/2.41c6c5b7.chunk.css"
+  },
+  {
+    "revision": "1456532929027842fa4b",
+    "url": "/iqvia/static/css/main.cf629934.chunk.css"
+  },
+  {
+    "revision": "f9e3753e07529f05f1d9",
+    "url": "/iqvia/static/js/2.bfae2c59.chunk.js"
+  },
+  {
+    "revision": "1456532929027842fa4b",
+    "url": "/iqvia/static/js/main.e795fa82.chunk.js"
+  },
+  {
+    "revision": "02bd36eb3f608552e58e",
+    "url": "/iqvia/static/js/runtime~main.6762d817.js"
+  },
+  {
+    "revision": "8c8666d923cb90d21214c243c05c8275",
+    "url": "/iqvia/static/media/apollo-logo-gray.8c8666d9.svg"
+  },
+  {
+    "revision": "834a884a4ca991b5668b30a6aaec88f4",
+    "url": "/iqvia/static/media/logo-img.834a884a.png"
+  },
+  {
+    "revision": "49d6f507e344cccf10ea98ac6bc88516",
+    "url": "/iqvia/static/media/proxima_nova_black-webfont.49d6f507.ttf"
+  },
+  {
+    "revision": "8d27b079007cee29ea257abfbcdfab27",
+    "url": "/iqvia/static/media/proxima_nova_black-webfont.8d27b079.woff"
+  },
+  {
+    "revision": "8f9ac04e398bdd90e1bfe9a5b589b73b",
+    "url": "/iqvia/static/media/proxima_nova_black-webfont.8f9ac04e.woff2"
+  },
+  {
+    "revision": "4ba7c7cd2df0ec1ba9ad8aad401e6c8b",
+    "url": "/iqvia/static/media/proxima_nova_black_it-webfont.4ba7c7cd.woff2"
+  },
+  {
+    "revision": "c2992e2f6075d02ba79a9911fc673518",
+    "url": "/iqvia/static/media/proxima_nova_black_it-webfont.c2992e2f.ttf"
+  },
+  {
+    "revision": "d7e90b4030b51c6a9630d8a70f32ac1f",
+    "url": "/iqvia/static/media/proxima_nova_black_it-webfont.d7e90b40.woff"
+  },
+  {
+    "revision": "21d81153447544b98ae75ea8b481e072",
+    "url": "/iqvia/static/media/proxima_nova_bold-webfont.21d81153.woff2"
+  },
+  {
+    "revision": "c7ebd95da278301f79c43e27b88303c5",
+    "url": "/iqvia/static/media/proxima_nova_bold-webfont.c7ebd95d.woff"
+  },
+  {
+    "revision": "fb0a83318257bd109c855983d27a4886",
+    "url": "/iqvia/static/media/proxima_nova_bold-webfont.fb0a8331.ttf"
+  },
+  {
+    "revision": "320686f71a303520ee916d24ac5f3cb6",
+    "url": "/iqvia/static/media/proxima_nova_bold_it-webfont.320686f7.woff2"
+  },
+  {
+    "revision": "5026e4e9b52da22e74719952ea56d3c4",
+    "url": "/iqvia/static/media/proxima_nova_bold_it-webfont.5026e4e9.ttf"
+  },
+  {
+    "revision": "9400d7cd1c2ee9b2c75b6445901144ef",
+    "url": "/iqvia/static/media/proxima_nova_bold_it-webfont.9400d7cd.woff"
+  },
+  {
+    "revision": "3f8f8da7ee9381b7e5416d3bbfd0ecd7",
+    "url": "/iqvia/static/media/proxima_nova_ebold-webfont.3f8f8da7.ttf"
+  },
+  {
+    "revision": "6c7639e39a6f99755eaf7eb01d13094c",
+    "url": "/iqvia/static/media/proxima_nova_ebold-webfont.6c7639e3.woff"
+  },
+  {
+    "revision": "858cac3f30b82658cb66d6940d6a46e9",
+    "url": "/iqvia/static/media/proxima_nova_ebold-webfont.858cac3f.woff2"
+  },
+  {
+    "revision": "12c9958f25adab36e6c36cdb68aee0a3",
+    "url": "/iqvia/static/media/proxima_nova_ebold_it-webfont.12c9958f.woff"
+  },
+  {
+    "revision": "db063ef06ac2d6e1b54b0004e3c841e0",
+    "url": "/iqvia/static/media/proxima_nova_ebold_it-webfont.db063ef0.ttf"
+  },
+  {
+    "revision": "f11db97f1c3898c05d2d5484c684969f",
+    "url": "/iqvia/static/media/proxima_nova_ebold_it-webfont.f11db97f.woff2"
+  },
+  {
+    "revision": "620fa8a77a6fb6aa508014c58adedff5",
+    "url": "/iqvia/static/media/proxima_nova_light-webfont.620fa8a7.woff2"
+  },
+  {
+    "revision": "96dc24f9f32653b01e83318247be945b",
+    "url": "/iqvia/static/media/proxima_nova_light-webfont.96dc24f9.woff"
+  },
+  {
+    "revision": "e78b8f04b6dd8a0cdfa0325a8bd347ef",
+    "url": "/iqvia/static/media/proxima_nova_light-webfont.e78b8f04.ttf"
+  },
+  {
+    "revision": "dfe6d94668476aae5f200a1acf332132",
+    "url": "/iqvia/static/media/proxima_nova_light_it-webfont.dfe6d946.ttf"
+  },
+  {
+    "revision": "e082e87f5691bbd34c09e10bdf321b0d",
+    "url": "/iqvia/static/media/proxima_nova_light_it-webfont.e082e87f.woff"
+  },
+  {
+    "revision": "f63239e7b51ca9c02b629870726e9ad7",
+    "url": "/iqvia/static/media/proxima_nova_light_it-webfont.f63239e7.woff2"
+  },
+  {
+    "revision": "70bb6a936ba6ee9f0ec1a50493761e2d",
+    "url": "/iqvia/static/media/proxima_nova_medium-webfont.70bb6a93.ttf"
+  },
+  {
+    "revision": "813ef6002c2d177ccce5eda6f86bb5c0",
+    "url": "/iqvia/static/media/proxima_nova_medium-webfont.813ef600.woff"
+  },
+  {
+    "revision": "fe27e924e217e6bf049f6cc8af0b6dc5",
+    "url": "/iqvia/static/media/proxima_nova_medium-webfont.fe27e924.woff2"
+  },
+  {
+    "revision": "811684ffcf77d756f64739634d44029b",
+    "url": "/iqvia/static/media/proxima_nova_medium_it-webfont.811684ff.woff2"
+  },
+  {
+    "revision": "c840f7323276dcf371625697e973c1df",
+    "url": "/iqvia/static/media/proxima_nova_medium_it-webfont.c840f732.ttf"
+  },
+  {
+    "revision": "d337c6fa26acb2f3073821dbe245b1f5",
+    "url": "/iqvia/static/media/proxima_nova_medium_it-webfont.d337c6fa.woff"
+  },
+  {
+    "revision": "796f365cb90726497d4afe38f4de56bd",
+    "url": "/iqvia/static/media/proxima_nova_reg-webfont.796f365c.ttf"
+  },
+  {
+    "revision": "ae9d14d13666387c72df6d53007d41b8",
+    "url": "/iqvia/static/media/proxima_nova_reg-webfont.ae9d14d1.woff"
+  },
+  {
+    "revision": "df380301e43c13d5377a4cdffd1bb8eb",
+    "url": "/iqvia/static/media/proxima_nova_reg-webfont.df380301.woff2"
+  },
+  {
+    "revision": "167232393bce21f0cfba5ec78021d50d",
+    "url": "/iqvia/static/media/proxima_nova_reg_it-webfont.16723239.woff"
+  },
+  {
+    "revision": "93226df575173b365d186430ce5d3f35",
+    "url": "/iqvia/static/media/proxima_nova_reg_it-webfont.93226df5.ttf"
+  },
+  {
+    "revision": "c780405880298671e791d2a7f9bcceb3",
+    "url": "/iqvia/static/media/proxima_nova_reg_it-webfont.c7804058.woff2"
+  },
+  {
+    "revision": "05d0ef115275a0db915c1d4aac53576f",
+    "url": "/iqvia/static/media/proxima_nova_sbold-webfont.05d0ef11.woff2"
+  },
+  {
+    "revision": "087998e923a7f10ae9331989e3e04fdc",
+    "url": "/iqvia/static/media/proxima_nova_sbold-webfont.087998e9.ttf"
+  },
+  {
+    "revision": "491fd5fd4cab1b59c52409ebd31c75be",
+    "url": "/iqvia/static/media/proxima_nova_sbold-webfont.491fd5fd.woff"
+  },
+  {
+    "revision": "23c304a4d0cb4a8e3ca53bdeffae8ca1",
+    "url": "/iqvia/static/media/proxima_nova_sbold_it-webfont.23c304a4.woff2"
+  },
+  {
+    "revision": "594f443e2cc7beb636181e8a080dd7f7",
+    "url": "/iqvia/static/media/proxima_nova_sbold_it-webfont.594f443e.woff"
+  },
+  {
+    "revision": "b927889277447da8d6d439bcb571e31d",
+    "url": "/iqvia/static/media/proxima_nova_sbold_it-webfont.b9278892.ttf"
+  },
+  {
+    "revision": "56e8e474454c913351f63a9614c9c35c",
+    "url": "/iqvia/static/media/proxima_nova_thin-webfont.56e8e474.woff2"
+  },
+  {
+    "revision": "643270b36a8731d7f75f28b18096b75a",
+    "url": "/iqvia/static/media/proxima_nova_thin-webfont.643270b3.ttf"
+  },
+  {
+    "revision": "f8a1095c1c9e6e843c9bc0819f2f48a0",
+    "url": "/iqvia/static/media/proxima_nova_thin-webfont.f8a1095c.woff"
+  },
+  {
+    "revision": "503efbf3a98f5fa1c3b3eb002557ae11",
+    "url": "/iqvia/static/media/proxima_nova_thin_it-webfont.503efbf3.woff2"
+  },
+  {
+    "revision": "b8888f3e8b6777be12c5ec51a9100451",
+    "url": "/iqvia/static/media/proxima_nova_thin_it-webfont.b8888f3e.woff"
+  },
+  {
+    "revision": "ec501c52d9be96236273a567de8dea38",
+    "url": "/iqvia/static/media/proxima_nova_thin_it-webfont.ec501c52.ttf"
+  }
+]);
